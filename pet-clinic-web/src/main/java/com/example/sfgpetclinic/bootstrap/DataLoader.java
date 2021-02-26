b0e01d331835f24e7cd9061c56359426e0633c11
Praveen Mahato <praveenmahato147@gmail.com>
2021-02-26 22:35:08 +0530

    Added owner view page and controller

--- pet-clinic-web/src/main/java/com/example/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/example/sfgpetclinic/bootstrap/DataLoader.java
@@ -32,9 +32,9 @@ public class DataLoader implements CommandLineRunner {
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner1.setFirstName("Michael");
-        owner1.setLastName("Weston");
-        owner1.setId(2L);
+        owner2.setFirstName("Michael");
+        owner2.setLastName("Weston");
+        owner2.setId(2L);
 
         ownerService.save(owner2);
 
